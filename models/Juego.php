b38320d23bfd8644c975683612e3627079767720
backend.domcloud.dev <backend@nyc.domcloud.id>
2023-12-01 02:50:07 +0000

    Modelo Juego updated

--- models/Juego.php
+++ models/Juego.php
@@ -3,21 +3,23 @@ namespace Model;
 
 class Juego extends ActiveRecord{
     protected static $tabla = 'juegos';
-    protected static $columnasDB = ["id","nombre","autor","descripcion","link"];
+    protected static $columnasDB = ["id","nombre","autor","descripcion","link","imagen"];
     public  $id;
     public $nombre;
 	public $autor;
 	public $genero = array();
 	public $descripcion;
 	public $link;
+	public $imagen;
     public function __construct($args = [])
     {
         $this->id = $args["id"] ?? null;
         $this->nombre = $args["nombre"] ?? "";
         $this->autor = $args["autor"] ?? "";
-        $this->genero = $args["genero"] ?? "";
+        $this->genero = $args["genero"] ?? array();
         $this->descripcion = $args["descripcion"] ?? "";
         $this->link = $args["link"] ?? "";
+        $this->imagen = $args["imagen"] ?? "";
     }
     public function validar(){
         if(!$this->nombre){
